a7929d34899ce21bfb0906ea2129074229c4581f
281677160 <66651997+281677160@users.noreply.github.com>
2021-06-04 01:25:48 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -59,7 +59,7 @@ Ubuntu_lv="$(df -h | grep "/dev/mapper/ubuntu--vg-ubuntu--lv" | awk '{print $4}'
 echo "${Ubuntu_lv}" > pack
 CURRENT_Version="$(awk 'NR==1' pack)"
 CURRENT_Ver="60G"
-if [[ "60" -lt "40" ]];then
+if [[ "20" -lt "40" ]];then
 	TIME && read -p "是否增删插件? [y/N]: " YN
 	case ${YN:-N} in
 		[Yy])
