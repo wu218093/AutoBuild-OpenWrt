9752b7d2f1fc0c33c60d364353aaad8c2f16efd5
281677160 <66651997+281677160@users.noreply.github.com>
2021-06-04 02:08:04 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -61,7 +61,7 @@ Ubuntu_lv="$(df -h | grep "/dev/mapper/ubuntu--vg-ubuntu--lv" | awk '{print $4}'
 Ubuntu_kj="${Ubuntu_lv%?}"
 TIME g "您当前系统可用空间为${Ubuntu_kj}G"
 echo
-if [[ "${Ubuntu_kj}" -lt "80" ]];then
+if [[ "${Ubuntu_kj}" -lt "30" ]];then
 	TIME && read -p "可用空间小于 30G 编译容易出错,是否继续? [y/N]: " YN
 	case ${YN:-N} in
 		[Yy])
@@ -75,7 +75,6 @@ if [[ "${Ubuntu_kj}" -lt "80" ]];then
 		;;
 	esac
 fi
-echo
 echo
 echo
 rm -Rf openwrt
