1bf2e4a5f4cc6479e138da9541a2ed6bf39dd1ab
281677160 <66651997+281677160@users.noreply.github.com>
2021-06-04 20:21:10 +0800

    Update webluci.sh

--- webluci.sh
+++ webluci.sh
@@ -29,21 +29,6 @@ echo
 echo
 sleep 2s
 
-sudo apt-get update
-sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget curl swig rsync
-
-clear
-echo
-echo
-TIME g "|*******************************************|"
-TIME z "|                                           |"
-TIME b "|                                           |"
-TIME y "|           基本环境部署完成......          |"
-TIME z "|                                           |"
-TIME g "|                                           |"
-TIME z "|*******************************************|"
-echo
-
 if [ "$USER" == "root" ]; then
 	echo
 	echo
