20c6d0fb3e6bd841f3e0615af567b3ba2466a5f9
281677160 <66651997+281677160@users.noreply.github.com>
2021-06-04 21:54:07 +0800

    Update uu.sh

--- uu.sh
+++ uu.sh
@@ -4,3 +4,14 @@ if [ -z "$(ls -A "./openwrt/compile" 2>/dev/null)" ]; then
 else
 	source AutoBuild-OpenWrt/webluci.sh
 fi
+if [[ -n "$(ls -A "Lede_source" 2>/dev/null)" ]]; then
+          firmware="Lede_source"
+elif [[ -n "$(ls -A "Lienol_source" 2>/dev/null)" ]]; then
+          firmware="Lienol_source"
+elif [[ -n "$(ls -A "Project_source" 2>/dev/null)" ]]; then
+          firmware="Project_source"
+elif [[ -n "$(ls -A "Spirit_source" 2>/dev/null)" ]]; then
+          firmware="Spirit_source
+else
+	source AutoBuild-OpenWrt/webluci.sh
+fi
