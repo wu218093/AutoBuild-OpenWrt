4a5c455e4af3cb37ebcb2146e9c8ad9e5121a560
281677160 <66651997+281677160@users.noreply.github.com>
2021-06-04 21:56:53 +0800

    Update uu.sh

--- uu.sh
+++ uu.sh
@@ -6,13 +6,10 @@ else
 fi
 if [[ -n "$(ls -A "Lede_source" 2>/dev/null)" ]]; then
           firmware="Lede_source"
-fi
-if [[ -n "$(ls -A "Lienol_source" 2>/dev/null)" ]]; then
+elif [[ -n "$(ls -A "Lienol_source" 2>/dev/null)" ]]; then
           firmware="Lienol_source"
-fi
-if [[ -n "$(ls -A "Project_source" 2>/dev/null)" ]]; then
+elif [[ -n "$(ls -A "Project_source" 2>/dev/null)" ]]; then
           firmware="Project_source"
-fi
-if [[ -n "$(ls -A "Spirit_source" 2>/dev/null)" ]]; then
-          firmware="Spirit_source
+elif [[ -n "$(ls -A "Spirit_source" 2>/dev/null)" ]]; then
+          firmware="Spirit_source"
 fi
